eee4443ddf330b7590e7c035614b2b6cecfb0c49
vacation4444 <s104011571@gapp.nthu.edu.tw>
2025-03-21 22:54:45 +0800

    second commit

--- mblog/urls.py
+++ mblog/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from mysite.views import homepage, showpost
 
 urlpatterns = [
+    path('', homepage),
+    path('post/<slug:slug>/', showpost),
     path('admin/', admin.site.urls),
 ]
